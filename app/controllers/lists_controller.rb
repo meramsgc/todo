c85783affefed955d1e194460a1e5961a683cf36
meramsgc <add2ramesh@gmail.com>
2016-04-24 14:28:07 +0545

    add n delete method

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,21 +1,36 @@
 class ListsController < ApplicationController
 	def index
 	 @lists = List.all
-	 end
-	 
-	def show
 	end
 
 	def new
+		@list = List.new
 	end
 
 	def create
+		@list = List.create(lists_params)
+		if @list.save
+			redirect_to lists_path
+		else
+		    render("new")
+		    end	
 	end
 
-	def update
+	def edit
+		@list = List.find(params[:id])
 	end
 
 	def destroy
+
+		@list = List.find(params[:id])
+		@list.destroy
+		redirect_to lists_path
 	end
+
+
+	private
+    def lists_params
+    	    params.require(:list).permit(:title)
+         end
 
 end
